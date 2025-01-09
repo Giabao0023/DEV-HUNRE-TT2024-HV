4748b7eb4ca09dca7028b00ba24959739efa9a96
Giabao0023 <giabaohoang23@gmail.com>
2025-01-09 08:33:40 +0700

    DEV-HUNRE-TT2024-HV

---
+++ index.js
@@ -0,0 +1,36 @@
+// // index.js
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   // Xử lý carousel
+//   const carousel = document.querySelector("#carouselExampleCaptions");
+//   if (carousel) {
+//       const prevBtn = carousel.querySelector(".carousel-control-prev");
+//       const nextBtn = carousel.querySelector(".carousel-control-next");
+
+//       prevBtn.addEventListener("click", () => {
+//           // Xử lý khi nhấn Previous
+//           console.log("Previous slide");
+//       });
+
+//       nextBtn.addEventListener("click", () => {
+//           // Xử lý khi nhấn Next
+//           console.log("Next slide");
+//       });
+//   }
+
+//   // Các logic khác như xử lý nút Đăng Nhập/Đăng Ký
+//   const loginBtn = document.querySelector(".user-btn");
+//   const registerBtn = document.querySelector(".use-btn");
+
+//   if (loginBtn) {
+//       loginBtn.addEventListener("click", function () {
+//           alert("Bạn đã nhấn Đăng Nhập");
+//       });
+//   }
+
+//   if (registerBtn) {
+//       registerBtn.addEventListener("click", function () {
+//           alert("Bạn đã nhấn Đăng Ký");
+//       });
+//   }
+// });
